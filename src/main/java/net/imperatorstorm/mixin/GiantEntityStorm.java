730e509ea90e64226fff10a866ed966226b7c345
ImperatorStorm <30777770+ImperatorStorm@users.noreply.github.com>
2021-01-11 23:00:59 -0800

    test

--- src/main/java/net/imperatorstorm/mixin/GiantEntityStorm.java
+++ src/main/java/net/imperatorstorm/mixin/GiantEntityStorm.java
@@ -1,9 +1,17 @@
 package net.imperatorstorm.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.BreakDoorGoal;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.mob.GiantEntity;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.TurtleEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,13 +22,39 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.function.Predicate;
 
 @Mixin(GiantEntity.class)
-public class GiantEntityStorm extends MobEntity {
+public class GiantEntityStorm extends ZombieEntity {
+
 
 	protected GiantEntityStorm(EntityType<? extends MobEntity> entityType, World world) {
-		super(entityType, world);
+		super((EntityType<? extends ZombieEntity>) entityType, world);
 	}
 
-	@Inject(at = @At("TAIL"), method = "<init>(EntityType<? extends ZombieEntity> entityType, World world)V")
+	@Inject(at = @At("TAIL"), method = "<init>(Ljava/lang/Object;Ljava/lang/Object;)V")
+	public void tickMovement() {
+		if (this.isAlive()) {
+			boolean bl = this.burnsInDaylight() && this.isAffectedByDaylight();
+			if (bl) {
+				ItemStack itemStack = this.getEquippedStack(EquipmentSlot.HEAD);
+				if (!itemStack.isEmpty()) {
+					if (itemStack.isDamageable()) {
+						itemStack.setDamage(itemStack.getDamage() + this.random.nextInt(2));
+						if (itemStack.getDamage() >= itemStack.getMaxDamage()) {
+							this.sendEquipmentBreakStatus(EquipmentSlot.HEAD);
+							this.equipStack(EquipmentSlot.HEAD, ItemStack.EMPTY);
+						}
+					}
+
+					bl = false;
+				}
+
+				if (bl) {
+					this.setOnFireFor(8);
+				}
+			}
+		}
+
+		super.tickMovement();
+	}
 	private void init(CallbackInfo DOOR_BREAK_DIFFICULTY_CHECKER){
 		BreakDoorGoal breakDoorsGoal = new BreakDoorGoal(this, (Predicate<Difficulty>) DOOR_BREAK_DIFFICULTY_CHECKER);
 	}
